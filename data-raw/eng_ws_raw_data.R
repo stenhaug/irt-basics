d11b1ee09edeaac61c523bf52ba217afeb677374
Ben Stenhaug <benastenhaug@gmail.com>
2019-12-16 18:20:38 -0800

    Initial commit

---
+++ data-raw/eng_ws_raw_data.R
@@ -0,0 +1,24 @@
+# cleans data-raw/eng_ws_raw_data.Rds and outputs as data-clean/animals_english.csv 
+# eng_ws_raw_data.Rds obtained from https://github.com/langcog/wordbank-book/tree/master/data/psychometrics
+# which is used in context of book https://langcog.github.io/wordbank-book/psychometrics.html
+
+library(tidyverse)
+
+# note this ends with .Rds but read_rds doesn't work with it so I think it's actually a .Rdata file
+# my understanding is that Rds files should be on object that can be loaded (and assigned) with read_rds 
+# see here for more https://stackoverflow.com/questions/21370132/r-data-formats-rdata-rda-rds-etc
+load("data-raw/eng_ws_raw_data.Rds")
+
+english_words <- 
+  eng_ws %>% 
+  select(child = data_id, produces = value, word = definition, sex, age) %>% 
+  mutate(
+    produces = ifelse(produces == "produces", 1, 0), 
+    sex = as.character(sex),
+    word = word %>% str_remove(" \\(animal\\)")
+  )
+
+english_words %>% 
+  spread(word, produces) %>% 
+  na.omit() %>% 
+  write_csv("data-clean/english_words.csv")
